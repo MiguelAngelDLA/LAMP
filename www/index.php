ffda4a2b5e1d08246400a9ff3cb1db846008af67
MiguelAngelDLA <miguelangeldla14@gmail.com>
2026-04-23 12:44:09 -0600

    batman

---
+++ www/index.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'db'; // Nombre del servicio en docker-compose
+$user = getenv('MYSQL_USER') ?: 'usuario_pba';
+$pass = getenv('MYSQL_PASSWORD') ?: 'password_usuario';
+$db   = getenv('MYSQL_DATABASE') ?: 'mi_base_de_datos';
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
+echo "¡Conexión exitosa a MySQL en Google Cloud!";
+?>
